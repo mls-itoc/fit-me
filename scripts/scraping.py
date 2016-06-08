7d292aa3006767cce18de5a9285bc9a60deb3dc4
ino_h <syslink.h.inoue@gmail.com>
2016-06-09 06:18:30 +0900

    打順データの取得スクリプトを作成

---
+++ scripts/scraping.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+import requests
+import re
+from pyquery import PyQuery as pq
+
+f = open("order.csv", "w")
+for year in range(9,16):
+    dom = pq(requests.get("http://baseball-data.com/{0}/lineup/c.html".format(year)).text.encode('utf-8'))
+    for tr in dom('.lineup tr').items():
+        vals = []
+        tds = tr.children('td')
+        for td in [td for td in tds]:
+            if len(td.getchildren()) == 0:
+                vals.append(td.text)
+            else:
+                vals.append(td.getchildren()[0].text)
+        if len(vals) > 0:
+            month_day = re.split('[月|日]',vals[0])[:2]
+            vals.remove(vals[0])
+            vals.insert(0, month_day[1])
+            vals.insert(0, month_day[0])
+            vals.insert(0, str(2000+year))
+            f.write('"' + '","'.join(vals) + '"')
+            f.write('\n')
+f.close()
